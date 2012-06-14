7ecd23233f1a14843fc5f37a131e232572812bd3
Genzj <zj0512@gmail.com>
2012-06-14 22:31:22 +0800

    ADD remember last successfully downloaded photo. return if no update.

--- ng-wallpaper.bash
+++ ng-wallpaper.bash
@@ -2,12 +2,14 @@
 
 #TODO
 # add option to change background fill mode
+
 photo_page="http://photography.nationalgeographic.com/photography/photo-of-the-day"
 user_page=""
 download_tool="curl -s"
 download_output_arg="-o "
 save_file=""
 gnome_version=3
+ng_last_record=~/.ng_last_wallpaper
 
 trait_regex='<a[^>]*>\s*Download Wallpaper[^<]*</a>'
 
@@ -17,7 +19,7 @@ fallback_regex='<img[^>]+src="[^"]+.jpg"[^>]+width="9[0-9][0-9]"[^>]*/>'
 my_name=`basename $0`
 
 function usage() {
-	echo "${my_name} [-23fhns] [-u URL]"
+	echo "${my_name} [-23fhlns] [-u URL]"
 	echo
 	echo "Download and change wallpaper from National Geography official website"
 	echo ""
@@ -27,6 +29,13 @@ function usage() {
 	echo "  -f      -- Force download even if the resolution"
 	echo "             of photo is not designed for wallpaper "
 	echo "  -h      -- Show this message "
+	echo "  -l FILE -- Check filename of last downloaded photo"
+	echo "             stored in FILE. Exit if filename read from"
+	echo "             URL is same."
+	echo "             Also record photo name to FILE if current"
+	echo "             download succeeds. FILE is $ng_last_record"
+	echo "             by default. An empty string to FILE disables"
+	echo "             checking and recording."
 	echo "  -n      -- Download only, keep current desktop"
 	echo "             background*"
 	echo "  -s      -- Save photo by its original name"
@@ -60,7 +69,7 @@ function setwallpaper() {
 }
 
 # Parse options 
-TEMP=`getopt -o 23fhnsu: -n "${my_name}" -- "$@"`
+TEMP=`getopt -o 23fhl:nsu: -n "${my_name}" -- "$@"`
 
 if [ $? != 0 ] ; then echo "Try \"$my_name -h\" for more information." >&2 ; exit 1 ; fi
 
@@ -72,6 +81,7 @@ do
 		-2|-3|-n) gnome_version=${1##*-}; shift ;;
 		-f) force='y'; shift;;
 		-h) usage; exit 0;;
+		-l) if [[ -n "$2" ]]; then ng_last_record="$2"; fi; shift 2 ;;
 		-s) save_file="y" ; shift ;;
 		-u) if [[ -n "$2" ]]; then photo_page="$2"; fi; shift 2 ;;
 		--) shift ; break ;;
@@ -107,7 +117,13 @@ fi
 # Download wallpaper
 download_output_file=${photo_url##*/}
 
+if [[ -n $ng_last_record && -r $ng_last_record ]] 
+then
+	read lastfile < $ng_last_record
+	if [[ $lastfile == $download_output_file ]]; then exit 0; fi
+fi
 echo "Downloading wallpaper file $photo_url"
+
 $download_tool $photo_url $download_output_arg $download_output_file
 wallpaper_file="`pwd -P`/wallpaper.${download_output_file##*.}"
 if [[ $save_file != 'y' ]] ; then 
@@ -115,5 +131,8 @@ if [[ $save_file != 'y' ]] ; then
 else
 	cp $download_output_file "$wallpaper_file"
 fi
+
+# Download successfully, make a record
+if [[ -n $ng_last_record ]]; then echo $download_output_file > $ng_last_record ; fi
 
 if [[ $gnome_version != 'n' ]] ; then setwallpaper $gnome_version $wallpaper_file ; fi
